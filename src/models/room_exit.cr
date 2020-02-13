19bea8ffce4ebdc08d25540adac4ff2b3f77ce57
Mike Janger <inchingforward@gmail.com>
2020-02-12 22:01:38 -0600

    Remove to_s.

--- src/models/room_exit.cr
+++ src/models/room_exit.cr
@@ -1,8 +1,4 @@
 class RoomExit
     def initialize(@from_room_key : String, @to_room_key : String, @to_room_label : String)
     end
-
-    def to_s(io)
-        io << "#{@to_room_label}: #{@to_room_key}"
-    end
 end
